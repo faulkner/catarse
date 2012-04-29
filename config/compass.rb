4d249ba4b7816bde1564db5ed56f15b554d2381d
Chris Faulkner <thefaulkner@gmail.com>
2012-04-29 14:55:57 -0700

    Asset pipeline conversion cleanup.

--- config/compass.rb
+++ config/compass.rb
@@ -4,13 +4,13 @@ project_type = :rails
 project_path = Compass::AppIntegration::Rails.root
 # Set this to the root of your project when deployed:
 http_path = "/"
-if Rails.env == "production"
+http_images_path = '/assets/'
+environment = Compass::AppIntegration::Rails.env
+if environment == "production"
   css_dir = "tmp/stylesheets"
 else
-  css_dir = "public/stylesheets"
+  css_dir = "app/assets/stylesheets"
 end
 sass_dir = "app/stylesheets"
-environment = Compass::AppIntegration::Rails.env
 # To enable relative paths to assets via compass helper functions. Uncomment:
 # relative_assets = true
-
